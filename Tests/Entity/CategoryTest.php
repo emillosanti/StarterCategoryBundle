f65fd38d17d5c0edf1cf8573dc3a3f454fbe343b
emillo <emil.the.studious@gmail.com>
2016-05-05 15:17:12 +0800

    tests for category constraints

--- Tests/Entity/CategoryTest.php
+++ Tests/Entity/CategoryTest.php
@@ -2,6 +2,8 @@
 
 namespace Starter\CategoryBundle\Tests\Entity;
 
+use Symfony\Component\Validator\Validation;
+
 class CategoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testEntity()
@@ -35,24 +37,60 @@ class CategoryTest extends \PHPUnit_Framework_TestCase
             'category entity has no getDescription method'
         );
 
-         //check methods work as expected
-         $name = 'This is a category name.';
+        //check methods work as expected
+        $name = 'This is a category name.';
 
-         $category->setName($name);
-         $this->assertEquals($name, $category->getName(),
-             'category description set through setName is not the same as name retrieved through getName'
-         );
+        $category->setName($name);
+        $this->assertEquals($name, $category->getName(),
+            'category description set through setName is not the same as name retrieved through getName'
+        );
 
-         $description = 'This is a category description.';
+        $description = 'This is a category description.';
 
-         $category->setDescription($description);
-         $this->assertEquals($description, $category->getDescription(),
-             'category description set through setDescription is not the same as description retrieved through getDescription'
-         );
-     }
+        $category->setDescription($description);
+        $this->assertEquals($description, $category->getDescription(),
+            'category description set through setDescription is not the same as description retrieved through getDescription'
+        );
+    }
 
-     public function getCategoryEntity()
-     {
-         return new \Starter\CategoryBundle\Entity\Category();
-     }
+    public function testCategoryConstraints()
+    {
+        $validator = Validation::createValidatorBuilder()->enableAnnotationMapping()->getValidator();
+
+        $category1 = $this->getCategoryEntity();
+        $errors1 = $validator->validate($category1);
+
+        $this->assertEquals(2, count($errors1),
+            'empty category should have 2 errors'
+        );
+
+        $category1->setName('category name');
+        $category1->setDescription('category description');
+        $errors2 = $validator->validate($category1);
+
+        $this->assertEquals(0, count($errors2),
+            'category with name and description should be valid'
+        );
+
+        $category2 = $this->getCategoryEntity();
+        $category2->setName('category name');
+        $errors3 = $validator->validate($category2);
+
+        $this->assertGreaterThanOrEqual(0, count($errors3),
+            'category with name only should not be valid'
+        );
+
+        $category3 = $this->getCategoryEntity();
+        $category3->setDescription('category description');
+        $errors4 = $validator->validate($category3);
+
+        $this->assertGreaterThanOrEqual(0, count($errors4),
+            'category with description only should not be valid'
+        );
+    }
+
+    public function getCategoryEntity()
+    {
+        return new \Starter\CategoryBundle\Entity\Category();
+    }
 }
